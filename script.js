d18c92fb3d79d40402e972032e6a60b7c907f5a9
hackgames13 <159486747+hackgames13@users.noreply.github.com>
2024-02-16 12:27:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const button = document.getElementById('ok');
+const button2 = document.getElementById('Download');
 
 function onclickbutton() {
-    document.close();
     window.open('./good', width=200, height=200);
+}
+function onclickbutton2() {
+    window.open('./download');
 }
 
 button.addEventListener('click',onclickbutton);
+button2.addEventListener('click',onclickbutton2);
